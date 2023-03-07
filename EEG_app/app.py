96f687c01edad0e0730f1669aa63b91d7ce9bf7c
jessiexiong <jessiexiong2000@yahoo.ca>
2023-03-07 13:49:57 -0500

    rough outline of frontend

--- EEG_app/app.py
+++ EEG_app/app.py
@@ -1,10 +1,18 @@
-import numpy as np
 from flask import Flask, request, render_template
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+from tensorflow import keras
+
+import numpy as np
+import pandas as pd
+
+import base64
+import collections
+import io
 import pickle
 
 app = Flask(__name__)
 
-# model = pickle.load(open('models/model.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -13,16 +21,37 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    # int_features = [int(x) for x in request.form.values()]
-    # features = [np.array(int_features)]
-    # prediction = model.predict(features)
-    # result = prediction[0]
-    string = request.form['filepath']
-    if string=="f":
-        result = "BING BONG you are excited 🥺"
-    else:
-        result = "BING BONG you are neutral 😩"
-    return render_template('index.html', prediction=result)
+    data = pd.read_csv('/Users/jessiexiong/Desktop/test/test.csv')
+    x_test = data.drop('Label', axis=1).copy()
+
+    reconstructed_model = keras.models.load_model("modelbci_0225")
+
+    x_pred = np.array(list(map(lambda x: np.argmax(x), reconstructed_model.predict(x_test))))
+    counter = collections.Counter(x_pred)
+
+    fig = Figure()
+    ax = fig.add_axes([0, 0, 1, 1])
+    ax.axis('equal')
+
+    # get the data for the pie chart
+    labels, data = [], []
+    for item in counter.most_common():
+        if item[0] == 2:
+            labels.append("Concentrated")
+        elif item[0] == 0:
+            labels.append("Relaxed")
+        data.append(item[1])
+
+    colors = ['#DD7596', '#8EB897']
+
+    ax.pie(data, labels=labels, colors = colors, autopct='%1.0f%%')
+
+    img = io.BytesIO()
+    FigureCanvas(fig).print_png(img)
+    img.seek(0)
+    chart_url = base64.b64encode(img.getvalue()).decode()
+
+    return render_template('index.html', chart_url=chart_url)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
